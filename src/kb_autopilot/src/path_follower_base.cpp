2d96e9dd2825e54254e1d7ecf95750c6477318ee
Gary Ellingson <gellings13@gmail.com>
2018-06-05 13:53:42 -0600

    initial hack at the path follower, moved some stuff around and builing

--- src/kb_autopilot/src/path_follower_base.cpp
+++ src/kb_autopilot/src/path_follower_base.cpp
@@ -1,27 +1,26 @@
 #include "path_follower_base.h"
 #include "path_follower_example.h"
 
-namespace rosplane
+namespace kb_autopilot
 {
 
 path_follower_base::path_follower_base():
   nh_(ros::NodeHandle()),
   nh_private_(ros::NodeHandle("~"))
 {
-  vehicle_state_sub_ = nh_.subscribe<rosplane_msgs::State>("state", 1, &path_follower_base::vehicle_state_callback, this);
-  current_path_sub_ = nh_.subscribe<rosplane_msgs::Current_Path>("current_path", 1,
+  vehicle_state_sub_ = nh_.subscribe<kb_autopilot::State>("state", 1, &path_follower_base::vehicle_state_callback, this);
+  current_path_sub_ = nh_.subscribe<kb_autopilot::Current_Path>("current_path", 1,
                       &path_follower_base::current_path_callback, this);
 
-
-  nh_private_.param<double>("CHI_INFTY", params_.chi_infty, 1.0472);
+  nh_private_.param<double>("CHI_INFTY", params_.psi_infty, 1.0472);
   nh_private_.param<double>("K_PATH", params_.k_path, 0.025);
   nh_private_.param<double>("K_ORBIT", params_.k_orbit, 4.0);
 
-  func_ = boost::bind(&path_follower_base::reconfigure_callback, this, _1, _2);
-  server_.setCallback(func_);
+//  func_ = boost::bind(&path_follower_base::reconfigure_callback, this, _1, _2);
+//  server_.setCallback(func_);
 
   update_timer_ = nh_.createTimer(ros::Duration(1.0/update_rate_), &path_follower_base::update, this);
-  controller_commands_pub_ = nh_.advertise<rosplane_msgs::Controller_Commands>("controller_commands", 1);
+  controller_commands_pub_ = nh_.advertise<kb_autopilot::Controller_Commands>("controller_commands", 1);
 
   state_init_ = false;
   current_path_init_ = false;
@@ -35,34 +34,30 @@ void path_follower_base::update(const ros::TimerEvent &)
   if (state_init_ == true && current_path_init_ == true)
   {
     follow(params_, input_, output);
-    rosplane_msgs::Controller_Commands msg;
-    msg.chi_c = output.chi_c;
-    msg.Va_c = output.Va_c;
-    msg.h_c = output.h_c;
-    msg.phi_ff = output.phi_ff;
+    kb_autopilot::Controller_Commands msg;
+    msg.psi_c = output.psi_c;
+    msg.u_c = output.u_c;
     controller_commands_pub_.publish(msg);
   }
 }
 
-void path_follower_base::vehicle_state_callback(const rosplane_msgs::StateConstPtr &msg)
+void path_follower_base::vehicle_state_callback(const kb_autopilot::StateConstPtr &msg)
 {
-  input_.pn = msg->position[0];               /** position north */
-  input_.pe = msg->position[1];               /** position east */
-  input_.h = -msg->position[2];                /** altitude */
-  input_.chi = msg->chi;
-  input_.Va = msg->Va;
+  input_.pn = msg->p_north;               /** position north */
+  input_.pe = msg->p_east;               /** position east */
+  input_.psi = msg->psi;
+  input_.u = msg->u;
 
   state_init_ = true;
 }
 
-void path_follower_base::current_path_callback(const rosplane_msgs::Current_PathConstPtr &msg)
+void path_follower_base::current_path_callback(const kb_autopilot::Current_PathConstPtr &msg)
 {
   if (msg->path_type == msg->LINE_PATH)
     input_.p_type = path_type::Line;
   else if (msg->path_type == msg->ORBIT_PATH)
     input_.p_type = path_type::Orbit;
 
-  input_.Va_d = msg->Va_d;
   for (int i = 0; i < 3; i++)
   {
     input_.r_path[i] = msg->r[i];
@@ -74,18 +69,18 @@ void path_follower_base::current_path_callback(const rosplane_msgs::Current_Path
   current_path_init_ = true;
 }
 
-void path_follower_base::reconfigure_callback(rosplane::FollowerConfig &config, uint32_t level)
-{
-  params_.chi_infty = config.CHI_INFTY;
-  params_.k_path = config.K_PATH;
-  params_.k_orbit = config.K_ORBIT;
-}
+//void path_follower_base::reconfigure_callback(kb_autopilot::FollowerConfig &config, uint32_t level)
+//{
+//  params_.chi_infty = config.CHI_INFTY;
+//  params_.k_path = config.K_PATH;
+//  params_.k_orbit = config.K_ORBIT;
+//}
 } //end namespace
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "rosplane_path_follower");
-  rosplane::path_follower_base *path = new rosplane::path_follower_example();
+  ros::init(argc, argv, "path_follower");
+  kb_autopilot::path_follower_base *path = new kb_autopilot::path_follower_example();
 
   ros::spin();
 
